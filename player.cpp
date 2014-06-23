a4a63fe1a54bd1f723fbe0369536122ea1d0aa9b
timothy lamb <trash80@gmail.com>
2014-06-23 13:50:46 -0700

    Added license info missing from initial commit.

--- player.cpp
+++ player.cpp
@@ -1,4 +1,22 @@
-#include "player.h"
+/*
+ * Player class File
+ * Copyright (C) 2014 Timothy Lamb - trash80.com
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ #include "player.h"
 
 uint8_t m_project[PROJECT_SIZE];
 uint8_t m_song[SONG_SIZE][NUM_TRACKS];
